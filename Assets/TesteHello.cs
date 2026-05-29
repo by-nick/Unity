bdaffb443e110698ea75ed00fada81cbee51367a
by-nick <laribrito78@gmail.com>
2026-05-29 14:53:18 -0300

    primeiros passos na Unity

---
+++ Assets/TesteHello.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TesteHello : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        Debug.Log("Hello Nick!");
+    }
+
+    // Update is called once per frame
+    
+}
