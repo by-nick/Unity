eb8787f5aed891bbcf2d0c1660ca2d06467639cb
by-nick <laribrito78@gmail.com>
2026-06-02 20:08:13 -0300

    Escovando os dentes com c#

---
+++ learningCSharp/exercicios/01a-BrushTeeth/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Pegar escova");
+Console.WriteLine("Colocar pasta de dente");
+Console.WriteLine("Molhar");
+Console.WriteLine("Escovar os dentes");
+Console.WriteLine("Escovar a lingua");
+Console.WriteLine("Enxaguar");
+Console.WriteLine("Molhar o rosto");
+Console.WriteLine("FIM");
