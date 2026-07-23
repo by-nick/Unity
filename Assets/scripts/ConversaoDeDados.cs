dfc4e603ea9a89628a8c2d1a44c836bf2bf0dce3
by-nick <laribrito78@gmail.com>
2026-07-23 10:17:15 -0300

    Cast, ToString() e Convert.ToInt32();

--- Assets/scripts/ConversaoDeDados.cs
+++ Assets/scripts/ConversaoDeDados.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using System; 
 
 public class ConversaoDeDados : MonoBehaviour
 {
+
+    int carteira = 100;
+    float carteira2 = 10.5f;
+    bool teste = true;
+    string texto;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        texto = carteira.ToString();
+        Debug.Log(carteira);
+
+
         
     }
 
