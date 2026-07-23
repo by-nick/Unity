a682aa9dc96a0fd3a48caeac67a28dbc16b876a2
by-nick <laribrito78@gmail.com>
2026-07-23 13:52:44 -0300

    For

---
+++ Assets/scripts/For.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class For : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int x = 0; x <= 10; x++)
+        {
+            Debug.Log("Oi Nick, você consegue!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
