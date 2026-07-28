e46715be0f4b54079566896135b9878be4f5189b
by-nick <laribrito78@gmail.com>
2026-07-28 11:34:15 -0300

    Enum

--- Assets/scripts/Enum.cs
+++ Assets/scripts/Enum.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Enum : MonoBehaviour
 {
+    enum Ai { Atacar, Correr, Patrulhar }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Ai vilao01 = Ai.Patrulhar;
+        Ai vilao02 = Ai.Atacar;
+        Ai vilao03 = Ai.Correr;
+
+        Debug.Log(vilao02);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
