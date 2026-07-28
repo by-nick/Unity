9fe1ab4fb4370f5929fa933f442810a3f7ab8141
by-nick <laribrito78@gmail.com>
2026-07-27 21:09:38 -0300

    Array

---
+++ Assets/scripts/Array.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        Debug.Log(array[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
