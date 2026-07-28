77a45c21804c30258544323b95ad33c8260d0081
by-nick <laribrito78@gmail.com>
2026-07-27 21:30:42 -0300

    formas de alimentação do array

--- Assets/scripts/Array.cs
+++ Assets/scripts/Array.cs
@@ -5,8 +5,18 @@ public class Array : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int[] array = { 1, 2, 3, 4, 5 };
-        Debug.Log(array[0]);
+        //int[] array = { 1, 2, 3, 4, 5 };
+
+        //int[] array = new int[4]{4,3,2,1};
+
+        int [] array = new int [10];
+
+        for (int i = 0; i <= 9; i++)
+        {
+            array[i] = i+1;
+            Debug.Log(array[0]);
+        }
+        
     }
 
     // Update is called once per frame
