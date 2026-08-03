8ab3d0c327e813fdd1ebf62cf140ee8b960fed7c
by-nick <laribrito78@gmail.com>
2026-08-03 09:44:05 -0300

    classes

---
+++ Assets/scripts/Classes.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Classes : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+class Felino
+{
+    //Caracteristicas = atributos
+    string nome;
+    string corPelo;
+    int forca;
+
+
+    //acões = metodos
+    void atacar()
+    {
+
+        
+
+    }
+}
